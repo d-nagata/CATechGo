7e4689346c193c0264046f842bc35c4165015fd2
ikalemmon <nagatadaiki@MacBook-Pro-142.local>
2022-12-02 01:44:06 +0900

    routerにgorilla/muxを利用

--- handler/router/router.go
+++ handler/router/router.go
@@ -2,15 +2,15 @@ package router
 
 import (
 	"database/sql"
-	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/ikalemmon/CATechGo/handler"
 	"github.com/ikalemmon/CATechGo/service"
 )
 
-func NewRouter(db *sql.DB) *http.ServeMux {
+func NewRouter(db *sql.DB) *mux.Router {
 	// register routes
-	mux := http.NewServeMux()
+	mux := mux.NewRouter()
 	mux.HandleFunc("/user", handler.NewUserHandler(service.NewUserService(db)).ServeHTTP)
 	mux.HandleFunc("/gacha", handler.NewGachaHandler(service.NewGachaService(db)).ServeHTTP)
 	mux.HandleFunc("/character", handler.NewCharacterHandler(service.NewCharacterService(db)).ServeHTTP)
